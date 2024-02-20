6ce70ab249f269224f1014aa7594b3c30e5a74f1
James Boland <102449868+JRBoland@users.noreply.github.com>
2024-02-20 23:22:57 +1100

    pipes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,26 +8,39 @@ import {
   HttpException,
   HttpStatus,
   ForbiddenException,
+  ValidationPipe,
   BadRequestException,
   UseFilters,
+  UsePipes,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+//import { createCatSchema } from './schema/create-cat.schema';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+//import { ZodValidationPipe } from 'src/common/pipes/zod-validation.pipe';
+//import { ValidationPipe } from 'src/common/pipes/validation.pipe';
+import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
+  //@Post()
+  //@UsePipes(new ZodValidationPipe(createCatSchema))
+  //async create(@Body() createCatDto: CreateCatDto) {
+  //  this.catsService.create(createCatDto);
+  //}
+
   @Post()
+  @UsePipes(new ValidationPipe())
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
 
   @Get()
   async findAll(): Promise<Cat[]> {
-    throw new ForbiddenException();
+    return this.catsService.findAll();
   }
 
   @Get('httpexcep')
@@ -51,7 +64,7 @@ export class CatsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', new ParseIntPipe()) id: string) {
     return `This action returns a #${id} cat`;
   }
 
